5774a10a4df229daf0f0679f04fc9249e17c566c
tkana731 <tkana731@gmail.com>
2025-05-11 21:50:41 +0900

    mod

--- pages/api/venues/create.js
+++ pages/api/venues/create.js
@@ -22,19 +22,21 @@ export default async function handler(req, res) {
         });
 
         // 次の表示順を決定
-        const nextOrder = maxOrderVenue ? maxOrderVenue.displayOrder + 1 : 1;
+        const nextOrder = maxOrderVenue ? maxOrderVenue.display_order + 1 : 1;
 
         // 会場を登録
         const newVenue = await prisma.venue.create({
             data: {
                 name,
                 postal_code: postalCode || null,
-                prefecture_id: prefectureId || null,
                 address: address || null,
                 capacity: capacity || null,
                 official_url: officialUrl || null,
                 google_maps_url: googleMapsUrl || null,
-                display_order: nextOrder
+                display_order: nextOrder,
+                prefecture: {
+                    connect: { id: prefectureId }
+                }
             }
         });
 

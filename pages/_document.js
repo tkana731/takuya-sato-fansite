82dddd144214d2efa2174ed897fa66eba07a7245
tkana731 <tkana731@gmail.com>
2025-05-19 01:52:35 +0900

    add Google Search Consoleの検証用メタタグ

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta name="google-site-verification" content="1lM1ljGc3DT3pPl1rMmrZgr01LwlF38rPmRx6KcY8IE" />
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
